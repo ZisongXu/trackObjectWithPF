93da67e10c4cf64ba3e5a95d19353ed6999c380c
Zisong <1210652636@qq.com>
2022-09-09 19:23:48 +0100

    change method to record csv file

--- home/phd_project/code/error_file/draw_box.py
+++ home/phd_project/code/error_file/draw_box.py
@@ -15,13 +15,10 @@ import tf.transformations
 from PIL import Image
 from PIL import ImageDraw
 from cv_bridge import CvBridge
-from dope.inference.cuboid import Cuboid3d
-from dope.inference.cuboid_pnp_solver import CuboidPNPSolver
-from dope.inference.detector import ModelData, ObjectDetector
+
 from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import CameraInfo, Image as ImageSensor_msg
 from std_msgs.msg import String
-from vision_msgs.msg import Detection3D, Detection3DArray, ObjectHypothesisWithPose
 from visualization_msgs.msg import Marker, MarkerArray
 
 
@@ -55,86 +52,13 @@ class Ros_listener():
         im = im.convert('RGB')
         draw = Draw(im)
         
-        detection_array = Detection3DArray()
-        detection_array.header = image_msg.header
-
-        for m in self.models:
-            publish_belief_img = (self.pub_belief[m].get_num_connections() > 0)
-
-            # Detect object
-            results, im_belief = ObjectDetector.detect_object_in_image(
-                self.models[m].net,
-                self.pnp_solvers[m],
-                img,
-                self.config_detect,
-                make_belief_debug_img=publish_belief_img,
-                overlay_image=self.overlay_belief_images
-            )
-
-            # Publish pose and overlay cube on image
-            for i_r, result in enumerate(results):
-                if result["location"] is None:
-                    continue
-                loc = result["location"]
-                ori = result["quaternion"]
-
-                # transform orientation
-                transformed_ori = tf.transformations.quaternion_multiply(ori, self.model_transforms[m])
-
-                # rotate bbox dimensions if necessary
-                # (this only works properly if model_transform is in 90 degree angles)
-                dims = rotate_vector(vector=self.dimensions[m], quaternion=self.model_transforms[m])
-                dims = np.absolute(dims)
-                dims = tuple(dims)
-
-                pose_msg = PoseStamped()
-                pose_msg.header = image_msg.header
-                CONVERT_SCALE_CM_TO_METERS = 100
-                pose_msg.pose.position.x = loc[0] / CONVERT_SCALE_CM_TO_METERS
-                pose_msg.pose.position.y = loc[1] / CONVERT_SCALE_CM_TO_METERS
-                pose_msg.pose.position.z = loc[2] / CONVERT_SCALE_CM_TO_METERS
-                pose_msg.pose.orientation.x = transformed_ori[0]
-                pose_msg.pose.orientation.y = transformed_ori[1]
-                pose_msg.pose.orientation.z = transformed_ori[2]
-                pose_msg.pose.orientation.w = transformed_ori[3]
-
-                # Publish
-                self.pubs[m].publish(pose_msg)
-                self.pub_dimension[m].publish(str(dims))
-
-                # Add to Detection3DArray
-                detection = Detection3D()
-                hypothesis = ObjectHypothesisWithPose()
-                hypothesis.id = self.class_ids[result["name"]]
-                hypothesis.score = result["score"]
-                hypothesis.pose.pose = pose_msg.pose
-                detection.results.append(hypothesis)
-                detection.bbox.center = pose_msg.pose
-                detection.bbox.size.x = dims[0] / CONVERT_SCALE_CM_TO_METERS
-                detection.bbox.size.y = dims[1] / CONVERT_SCALE_CM_TO_METERS
-                detection.bbox.size.z = dims[2] / CONVERT_SCALE_CM_TO_METERS
-                detection_array.detections.append(detection)
-
-                # Draw the cube
-                if None not in result['projected_points']:
-                    points2d = []
-                    for pair in result['projected_points']:
-                        points2d.append(tuple(pair))
-                    draw.draw_cube(points2d, self.draw_colors[m])
-
-            # Publish the belief image
-            if publish_belief_img:
-                belief_img = self.cv_bridge.cv2_to_imgmsg(np.array(im_belief)[..., ::-1], "bgr8")
-                belief_img.header = camera_info.header
-                self.pub_belief[m].publish(belief_img)
-
-        # Publish the image with results overlaid
+        
         rgb_points_img = CvBridge().cv2_to_imgmsg(np.array(im)[..., ::-1], "bgr8")
         
         # draw.draw_cube(points)
         # rgb_points_img = CvBridge().cv2_to_imgmsg(np.array(im)[..., ::-1], "bgr8")
         
-        cv2.imshow("rgb_points_img:", rgb_points_img)
+#        cv2.imshow("rgb_points_img:", rgb_points_img)
         cv2.waitKey(3)
         
 class Draw(object):
@@ -202,7 +126,15 @@ class Draw(object):
         
 #cv_bridge = CvBridge()
 #img = cv_bridge.imgmsg_to_cv2(image_msg, "rgb8")
-points = [[0,0,0], [0,0,1], [1,0,1], [1,0,0], [0,1,0], [0,1,1], [1,1,1], [1,1,0]]
+points = [(270.73113541500015, 171.23152323217352), 
+          (241.46136527990785, 86.12035538275252), 
+          (355.92305269234504, 104.18411471846555), 
+          (393.4423106422681, 179.7875187171499), 
+          (252.5544934396236, 198.2046834515927), 
+          (226.773595371207, 114.98036848759719), 
+          (337.3676219356163, 128.83336788753047), 
+          (371.01054493868116, 202.87435265364252), 
+          (308.9311599655143, 146.9263616063306)]
 #draw = Draw()
 #draw.draw_cube(points2d, self.draw_colors[m])
 

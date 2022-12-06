140a31e9aaab2a4b69034e0ed66010f92bfbd7dc
Zisong <1210652636@qq.com>
2022-12-06 17:21:24 +0000

    add draw figure scripts

---
+++ home/catkin_ws/src/PBPF/scripts/error_file/generate_data_error.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 15:09:51 2022
+
+@author: 12106
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import copy
+import numpy as np
+flag_pos = True
+flag_ang = True
+flag_DOPE = True
+flag_PFPE = True
+flag_PFPM = False
+update_style_flag = "time"
+task_flag = "1b"
+loop_flag = 10
+
+file_name_obse_pos = update_style_flag+'_scene'+task_flag+'_obse_err_pos.csv'
+file_name_PFPE_pos = update_style_flag+'_scene'+task_flag+'_PFPE_err_pos.csv'
+file_name_PFPM_pos = update_style_flag+'_scene'+task_flag+'_PFPM_err_pos.csv'
+file_name_obse_ang = update_style_flag+'_scene'+task_flag+'_obse_err_ang.csv'
+file_name_PFPE_ang = update_style_flag+'_scene'+task_flag+'_PFPE_err_ang.csv'
+file_name_PFPM_ang = update_style_flag+'_scene'+task_flag+'_PFPM_err_ang.csv'
+file_name_pos = update_style_flag+'_scene'+task_flag+'_pos.csv'
+file_name_ang = update_style_flag+'_scene'+task_flag+'_ang.csv'
+error_file_name_DOPE_pos = "pos_error_DOPE"+"_scene"+task_flag
+error_file_name_DOPE_ang = "ang_error_DOPE"+"_scene"+task_flag
+error_file_name_PFPE_pos = "pos_error_PFPE"+"_scene"+task_flag
+error_file_name_PFPE_ang = "ang_error_PFPE"+"_scene"+task_flag
+error_file_name_PFPM_pos = "pos_error_PFPM"+"_scene"+task_flag
+error_file_name_PFPM_ang = "ang_error_PFPM"+"_scene"+task_flag
+# pos
+if flag_pos == True:
+    print("Ready to generate the data of pos")
+    if flag_DOPE == True:
+        print("Ready to generate the data of pos in DOPE")
+        pos_error_all_list = []
+        for j in range(loop_flag):
+            dataset = pd.read_csv(str(j+1)+file_name_obse_pos)
+            dataset.columns=["index","time","error","alg"]
+            datasetcopy = copy.deepcopy(dataset)
+            newdataset = pd.DataFrame(columns=['step','time','error','alg'],index=[])
+            # timedf = dataset['time']
+            pos_error = dataset['error']
+            for index in range(len(pos_error)):
+                pos_error_all_list.append(datasetcopy.error[index])
+        print("DOPE pos error mean:",np.mean(pos_error_all_list))
+        print("DOPE pos error  std:",np.std(pos_error_all_list))
+    if flag_PFPE == True:
+        print("Ready to generate the data of pos in PFPE")
+        pos_error_all_list = []
+        for j in range(loop_flag):
+            dataset = pd.read_csv(str(j+1)+file_name_PFPE_pos)
+            dataset.columns=["index","time","error","alg"]
+            datasetcopy = copy.deepcopy(dataset)
+            newdataset = pd.DataFrame(columns=['step','time','error','alg'],index=[])
+            # timedf = dataset['time']
+            pos_error = dataset['error']
+            for index in range(len(pos_error)):
+                pos_error_all_list.append(datasetcopy.error[index])
+        print("PFPE pos error mean:",np.mean(pos_error_all_list))
+        print("PFPE pos error  std:",np.std(pos_error_all_list))
+    if flag_PFPM == True:
+        print("Ready to generate the data of pos in PFPM")
+        pos_error_all_list = []
+        for j in range(loop_flag):
+            dataset = pd.read_csv(str(j+1)+file_name_PFPM_pos)
+            dataset.columns=["index","time","error","alg"]
+            datasetcopy = copy.deepcopy(dataset)
+            newdataset = pd.DataFrame(columns=['step','time','error','alg'],index=[])
+            # timedf = dataset['time']
+            pos_error = dataset['error']
+            for index in range(len(pos_error)):
+                pos_error_all_list.append(datasetcopy.error[index])
+        print("PFPM pos error mean:",np.mean(pos_error_all_list))
+        print("PFPM pos error  std:",np.std(pos_error_all_list))
+# ang
+if flag_ang == True:
+    print("Ready to integrate the data of ang")
+    if flag_DOPE == True:
+        print("Ready to generate the data of ang in DOPE")
+        ang_error_all_list = []
+        for j in range(loop_flag):
+            dataset = pd.read_csv(str(j+1)+file_name_obse_ang)
+            dataset.columns=["index","time","error","alg"]
+            datasetcopy = copy.deepcopy(dataset)
+            newdataset = pd.DataFrame(columns=['step','time','error','alg'],index=[])
+            # timedf = dataset['time']
+            ang_error = dataset['error']
+            for index in range(len(ang_error)):
+                ang_error_all_list.append(datasetcopy.error[index])
+        print("DOPE ang error mean:",np.mean(ang_error_all_list))
+        print("DOPE ang error  std:",np.std(ang_error_all_list))
+    if flag_PFPE == True:
+        print("Ready to generate the data of ang in PFPE")
+        ang_error_all_list = []
+        for j in range(loop_flag):
+            dataset = pd.read_csv(str(j+1)+file_name_PFPE_ang)
+            dataset.columns=["index","time","error","alg"]
+            datasetcopy = copy.deepcopy(dataset)
+            newdataset = pd.DataFrame(columns=['step','time','error','alg'],index=[])
+            # timedf = dataset['time']
+            ang_error = dataset['error']
+            for index in range(len(ang_error)):
+                ang_error_all_list.append(datasetcopy.error[index])
+        print("PFPE ang error mean:",np.mean(ang_error_all_list))
+        print("PFPE ang error  std:",np.std(ang_error_all_list))
+    if flag_PFPM == True:
+        print("Ready to generate the data of ang in PFPM")
+        ang_error_all_list = []
+        for j in range(loop_flag):
+            dataset = pd.read_csv(str(j+1)+file_name_PFPM_ang)
+            dataset.columns=["index","time","error","alg"]
+            datasetcopy = copy.deepcopy(dataset)
+            newdataset = pd.DataFrame(columns=['step','time','error','alg'],index=[])
+            # timedf = dataset['time']
+            ang_error = dataset['error']
+            for index in range(len(ang_error)):
+                ang_error_all_list.append(datasetcopy.error[index])
+        print("PFPM ang error mean:",np.mean(ang_error_all_list))
+        print("PFPM ang error  std:",np.std(ang_error_all_list))
+
+
+'''hue = 'alg','''
+#figure = sns.lineplot(x="time", y=newdataset.error,data=newdataset, ci=95)
+#svg_fig = figure.get_figure()
+#svg_fig.savefig("0_2_scene1a_err_pos.svg",format="svg")
---
+++ home/catkin_ws/src/PBPF/scripts/error_file/plotsns.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 24 15:09:51 2022
+
+@author: 12106
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import copy
+import math
+import numpy as np
+flag_plot_ang = False
+flag_plot_pos = True
+task_flag = "1b"
+update_style_flag = "time"
+
+if update_style_flag == "pose":
+    title_name = "Pose"
+elif update_style_flag == "time":
+    title_name = "Time"
+
+file_name_ang = update_style_flag+"_scene"+task_flag+"_ang"
+file_name_pos = update_style_flag+"_scene"+task_flag+"_pos"
+title_ang = "Rotational errors (rad) vs Time (s)"
+title_pos = "Positional errors (m) vs Time (s)"
+
+if flag_plot_ang == True:
+    print("Ready to plot the figure of ang")
+    dataset_ang = pd.read_csv(file_name_ang+'.csv')
+    dataset_ang.columns=["index","time","Rotational Error (rad)","alg"]
+    figure_ang = sns.lineplot(x="time", y="Rotational Error (rad)", data=dataset_ang, hue = 'alg', ci=95, legend=False, linewidth = 0.5)
+    figure_ang.set(xlabel = None, ylabel = None)
+    # figure_ang.set_xlabel(None)
+    x = range(0,23,2)
+    y = np.arange(0,math.pi/2.0+0.2, 0.2)
+    plt.xticks(x)
+    plt.yticks(y)
+    plt.tick_params(labelsize=15)
+    plt.xlim(0,23)
+    plt.ylim(0, math.pi / 2.0)
+    plt.title(title_ang, fontsize=16)
+    svg_fig_ang = figure_ang.get_figure()
+    svg_fig_ang.savefig(file_name_ang+".svg",format="svg")
+if flag_plot_pos == True:
+    print("Ready to plot the figure of pos")
+    ymax = 0.4/2.0
+    dataset_pos = pd.read_csv(file_name_pos+'.csv')
+    dataset_pos.columns=["index","time","Positional Error (m)","alg"]
+    figure_pos = sns.lineplot(x="time", y="Positional Error (m)", data=dataset_pos, hue = 'alg', ci=95, legend=False, linewidth = 0.5)
+    figure_pos.set(xlabel = None, ylabel = None)
+    x = range(0,23,2)
+    y = np.arange(0, 0.2, 0.02)
+    plt.xticks(x)
+    plt.yticks(y)
+    plt.tick_params(labelsize=15)
+    plt.xlim(0,23)
+    plt.ylim(0,ymax)
+    plt.title(title_pos, fontsize=16)
+    svg_fig_pos = figure_pos.get_figure()
+    svg_fig_pos.savefig(file_name_pos+".svg",format="svg")

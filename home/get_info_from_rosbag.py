88f16653ca1f93e179faba6b2d9e9a28db532acb
ZisongXu <1210652636@qq.com>
2023-02-01 19:32:28 +0000

    code update

---
+++ home/get_info_from_rosbag.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import yaml
+from rosbag.bag import Bag
+
+# info_dict = yaml.load(Bag('input.bag', 'r')._get_yaml_info())
+
+
+
+
+object_name = sys.argv[1]
+particle_number = int(sys.argv[2])
+scene_name = sys.argv[3]
+# run_alg_flag = sys.argv[4]
+rosbag_num = sys.argv[4]
+
+
+
+info_dict = yaml.load(Bag(os.path.expanduser(f"~/rosbag/new_rosbag/{object_name}_{scene_name}/{object_name}_{scene_name}_70_{rosbag_num}.bag"))._get_yaml_info(), Loader=yaml.FullLoader)
+dura = info_dict['duration'] - 9
+print(dura)

70ab8652898f02f7e0d5ad8da30fb11dd237a80e
ZisongXu <1210652636@qq.com>
2023-02-21 10:38:20 +0000

    code udpate

--- home/catkin_ws/src/PBPF/scripts/Visualisation_World.py
+++ home/catkin_ws/src/PBPF/scripts/Visualisation_World.py
@@ -322,8 +322,8 @@ if __name__ == '__main__':
     init_obse_flag = 0
     init_par_flag = 0
     init_esti_flag = 0
-    display_par_flag = False
-    display_esti_flag = True
+    display_par_flag = True
+    display_esti_flag = False
     display_gt_flag = True
     display_obse_flag = True
     object_name_list = parameter_info['object_name_list']

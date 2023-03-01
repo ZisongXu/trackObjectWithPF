525d97929e88eb3159f0390fce35c1b14614860f
ZisongXu <1210652636@qq.com>
2023-03-01 18:28:34 +0000

    code update

--- home/catkin_ws/src/PBPF/scripts/RecordError.py
+++ home/catkin_ws/src/PBPF/scripts/RecordError.py
@@ -161,12 +161,12 @@ def signal_handler(sig, frame):
             file_name_obse_ang = update_style_flag+'_obse_err_ang.csv'
             file_name_PBPF_ang = update_style_flag+'_PBPF_err_ang.csv'
 
-#            boss_obse_err_pos_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_obse_pos,index=0,header=0,mode='a')
-#            boss_obse_err_ang_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_obse_ang,index=0,header=0,mode='a')
-#            print("write obser file")
-#            boss_PBPF_err_pos_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_PBPF_pos,index=0,header=0,mode='a')
-#            boss_PBPF_err_ang_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_PBPF_ang,index=0,header=0,mode='a')
-#            print("write PBPF file")
+            boss_obse_err_pos_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_obse_pos,index=0,header=0,mode='a')
+            boss_obse_err_ang_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_obse_ang,index=0,header=0,mode='a')
+            print("write obser file")
+            boss_PBPF_err_pos_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_PBPF_pos,index=0,header=0,mode='a')
+            boss_PBPF_err_ang_df_list[obj_index].to_csv('catkin_ws/src/PBPF/scripts/'+err_file+'/'+str(file_name)+file_name_PBPF_ang,index=0,header=0,mode='a')
+            print("write PBPF file")
             
     if run_alg_flag == "CVPF":
         for obj_index in range(object_num):
@@ -383,8 +383,8 @@ if __name__ == '__main__':
                     boss_obse_err_pos_df_list[obj_index].loc[flag_record_obse] = [flag_record_obse, t_before_record - t_begin, err_opti_obse_pos, 'obse', obj_scene, particle_num, version]
                     boss_obse_err_ang_df_list[obj_index].loc[flag_record_obse] = [flag_record_obse, t_before_record - t_begin, err_opti_obse_ang, 'obse', obj_scene, particle_num, version]
                     flag_record_obse = flag_record_obse + 1
-                    boss_PBPF_err_pos_df_list[obj_index].loc[flag_record_PBPF] = [flag_record_PBPF, t_before_record - t_begin, err_opti_PBPF_pos, 'PBPF', obj_scene, particle_num, version]
-                    boss_PBPF_err_ang_df_list[obj_index].loc[flag_record_PBPF] = [flag_record_PBPF, t_before_record - t_begin, err_opti_PBPF_ang, 'PBPF', obj_scene, particle_num, version]
+                    boss_PBPF_err_pos_df_list[obj_index].loc[flag_record_PBPF] = [flag_record_PBPF, t_before_record - t_begin, err_opti_PBPF_pos, 'PBPF-V', obj_scene, particle_num, version]
+                    boss_PBPF_err_ang_df_list[obj_index].loc[flag_record_PBPF] = [flag_record_PBPF, t_before_record - t_begin, err_opti_PBPF_ang, 'PBPF-V', obj_scene, particle_num, version]
                     flag_record_PBPF = flag_record_PBPF + 1
                     
                 else:
